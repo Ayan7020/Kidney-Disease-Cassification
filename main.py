25779ffbb0ab977ccb3c0f53146c271d5de217e4
Ayan Shaikh <ayanshaikh.pr@gmail.com>
2023-12-15 18:52:46 +0530

    Data Ingestion

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-from src.CNN_Classifier import logger
+from CNN_Classifier import logger
+from CNN_Classifier.pipeline.stage01 import DataIngestionTrainingPipeline
 
-logger.info("hey")
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> Stage {STAGE_NAME} Started <<<<<<<")
+    logger.info(f">>>>Stage {STAGE_NAME} started <<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>Stage {STAGE_NAME} Completed <<<<\n\nx===============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/CNN_Classifier/utils/common.py
+++ src/CNN_Classifier/utils/common.py
@@ -34,3 +34,8 @@ def create_directories(path_to_directories: list, verbose=True):
         os.makedirs(path, exist_ok=True)
         if verbose:
             logger.info(f"created directory at: {path}")
+
+@ensure_annotations
+def get_size(path:Path) -> str:
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"            
